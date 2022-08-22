b999d58a6d89b59afe39f10780a3b911e4c2acb0
максим <send2max@bk.ru>
2022-08-22 11:37:28 +0300

    решил первую задачу

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int HonestNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(10, 100, 999);
+int honestNumbers = HonestNumbers(arr);
+PrintArray(arr);
+Console.WriteLine($" -> {honestNumbers}");
+
+// А как можно сделать что бы ответ о колличестве четных чисел в массиве был на одной строчкой с массивом?
+// Я что то не смог понять как это сделать, ну или как убрать этот автоматический перенос
+
+
--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
