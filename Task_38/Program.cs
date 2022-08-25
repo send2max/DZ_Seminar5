459d6cece6f03a1aa6f5425c49d01e1cd81a190f
Max <send2max@bk.ru>
2022-08-25 21:44:35 +0300

    Решил третью задачу

--- Task_38/Program.cs
+++ Task_38/Program.cs
@@ -2,3 +2,44 @@
 // Найдите разницу между максимальным и минимальным элементами массива.
 
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void DifferenceMaxMin(int[] array)
+
+{
+    int max = array[0];
+   int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+    }
+    Console.WriteLine("Разница максимального и минимального:  " + (max - min));
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+int[] arr = CreateArrayRndInt(5, 0, 100);
+PrintArray(arr);
+DifferenceMaxMin(arr);
+
+
